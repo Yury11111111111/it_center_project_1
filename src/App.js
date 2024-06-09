7c5958bf24fc47be67f443e1dfd65cdff9bd9679
unknown <yury.ovchinnkov@yandex.ru>
2024-06-09 18:17:27 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [login, setLogin] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  // todo need backend
+
+  const [allLogins, setAllLogins] = useState([
+    "78183483498141",
+    "12312412412463",
+  ]);
+
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const loginWrithing = (e) => {
+    setLogin(e.target.value);
+    console.log(login);
+  };
+
+  const loginCheck = () => {
+    if (allLogins.includes(login)) {
+      setIsAdmin(true);
+    } else {
+      alert("Неверный логин");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        {isAdmin !== true ? (
+          <button onClick={toggleModal} className="modal__button">
+            Авторизация
+          </button>
+        ) : (
+          <div className="header-placeholder"></div>
+        )}
       </header>
+      {isOpen && (
+        <div className="modal">
+          <div className="modal__content">
+            <span className="close" onClick={toggleModal}>
+              &times;
+            </span>
+            <div className="modal__content-login">
+              <input
+                type="text"
+                value={login}
+                onChange={loginWrithing}
+                className="login__input"
+                placeholder="Введите логин"
+              />
+              <button onClick={loginCheck} className="login__button">
+                Проверить
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
